e692dc554332f7268b2168c390f400557e00f2fa
David Song <David.Song0802@gmail.com>
2015-01-12 19:16:56 -0500

    added images

--- web/scripts/scroll.js
+++ web/scripts/scroll.js
@@ -1,3 +1,6 @@
+
+
+
 $(document).ready(function(){
     start();
 
@@ -12,3 +15,20 @@ $(window).scroll(function() {
 function start(){
     $("#lightbulb").fadeIn(3000);
 }
+
+
+
+// Function taken from http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    //return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    return ((docViewTop < elemTop) && (docViewBottom > elemBottom));
+}
+
+
