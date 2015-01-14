d37f88b8042f68b876256772845e70ac5922631a
Tony Lu <tonylu7712@hotmail.com>
2015-01-14 01:09:49 -0500

    added drawing line function, canvas and small changes in css,html

--- web/scripts/scroll.js
+++ web/scripts/scroll.js
@@ -1,17 +1,21 @@
 var numOfSections = 5;
 var fadeTime = 1000;
-
 var lightBulbVisible = false;
 var currentSection,
 	currentDisplayingPicture = 0,
 	offset = 0.5;
+	
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');	
+$(document).ready(function(){
+});
 
 
 $(window).scroll(function () {
 	var distanceFromTop = $(window).scrollTop(),
 		windowHeight = $(window).height();
 	currentSection = distanceFromTop / windowHeight;
-	console.log('Currently in: ' + currentSection + ' (int): ' + parseInt(currentSection));
+	//console.log('Currently in: ' + currentSection + ' (int): ' + parseInt(currentSection));
 
 	// prevent light bulb from loading first.
 	if (!lightBulbVisible) {
@@ -21,13 +25,31 @@ $(window).scroll(function () {
 
 	if (Math.abs(currentSection - currentDisplayingPicture) > offset) {
 		changeSection(Math.round(currentSection));
+		//drawLine($(window).width()/2.5,200, $(window).width()/2.5+160, 200);
+		$("#section1").css("position","fixed"); 
+		//drawLine($(window).width()/2.5,500, $(window).width()/2.5+160, 500);
 	}
 
 });
 
+function drawLine(startX,startY,endX,endY){
+	var amount = 0;
+	setInterval(function() {
+		amount += 0.05; 
+		if (amount > 1) amount = 1;
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.strokeStyle = "black";
+		context.moveTo(startX, startY);
+		context.lineTo(startX + (endX - startX) * amount, startY + (endY - startY) * amount);
+		context.lineWidth=2;
+		context.stroke();
+	}, 30);
+}
+
+
 function changeSection(newPicture) {
 	if (currentDisplayingPicture == newPicture) return;
-	console.log('changed to ' + newPicture);
+	//console.log('changed to ' + newPicture);
 
 	//if (currentDisplayingPicture) { // is not zero
 	//	document.getElementsByClassName()
