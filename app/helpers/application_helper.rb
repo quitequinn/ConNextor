9dc74718329beef1c14eb1482ba60e005149253a
Saul Gutierrez <saul.g.gutierrez@gmail.com>
2015-02-10 18:04:13 -0600

    Added user pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  # Returns the full title 
+  def full_title(page_title = '')
+    base_title = "ConNextor"
+    page_title.empty? ? base_title :  "#{page_title} | #{base_title}"
+  end
+  
 end
