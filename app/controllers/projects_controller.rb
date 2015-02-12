675c25602777c8eb9e9f2d905a72dc4508f5af72
Saul Gutierrez <saul.g.gutierrez@gmail.com>
2015-02-11 18:49:37 -0600

    Added projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,67 @@
+class ProjectsController < ApplicationController
+  before_action :logged_in_user, only: [:new] 
+  # before_action :correct_user,   only: [:edit, :update]
+
+  
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:success] = "Created project!"
+      redirect_to root_url
+    else
+      render "new"
+    end
+  end
+
+
+  def index
+    @allprojects = Project.paginate(page: params[:page])
+  end
+
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      flash[:success] = "Profile updated"
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+
+  def show
+    @project = Project.find(params[:id])
+  end  
+
+  
+  private
+  
+  def project_params
+    params.require(:project).permit(:name, :description )
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to log_in_url
+    end
+  end
+
+
+
+  # Confirms a logged-in project.
+  def logged_in_project
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to log_in_url
+    end
+  end
+  
+end
