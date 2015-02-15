e19901f7b8ffa9251406ddc39e10158c9c422c7a
David Song <David.Song0802@gmail.com>
2015-02-14 23:06:15 -0500

    added migration for products

---
+++ db/migrate/20150215000718_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :description
+      t.string :type
+      t.string :pitch
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207183919) do
+ActiveRecord::Schema.define(version: 20150215000718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "type"
+    t.string   "pitch"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
