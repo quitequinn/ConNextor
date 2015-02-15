28efc6224f6cf28db714b386dce62a7b1eb2abed
David Song <David.Song0802@gmail.com>
2015-02-15 03:40:42 -0500

    changes since scaffolding

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ActiveRecord::Base
+  validates :title, :short_description, presence: true
+  
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:short_description].any?
+  end
+
   # test "the truth" do
   #   assert true
   # end
