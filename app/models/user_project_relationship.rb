51fad2d0d45ff876f6f09551cf0887f2591491f2
David Song <David.Song0802@gmail.com>
2015-02-15 00:30:02 -0500

    reverted projects

--- app/models/user_project_relationship.rb
+++
@@ -1,6 +0,0 @@
-class UserProjectRelationship < ActiveRecord::Base
-  belongs_to :user, class_name: "User"
-  belongs_to :project, class_name: "Project
-  validates :user_id, presence: true
-  validates :project_id, presence: true"
-end
