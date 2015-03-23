5d36446be924a1863040dc3134b8810886f3229d
David Song <David.Song0802@gmail.com>
2015-03-22 21:14:51 -0400

    small tweaks on profile_intro, security check

--- app/controllers/profile_introductions_controller.rb
+++ app/controllers/profile_introductions_controller.rb
@@ -2,29 +2,30 @@
 class ProfileIntroductionsController < ApplicationController
   before_action :set_profile_introduction, only: [:show, :swap, :edit, :update, :destroy]
 
-  # GET /profile_introductions
-  # GET /profile_introductions.json
-  def index
-    @profile_introductions = ProfileIntroduction.all
-  end
+  # # GET /profile_introductions
+  # # GET /profile_introductions.json
+  # def index
+  #   @profile_introductions = ProfileIntroduction.all
+  # end
 
-  # GET /profile_introductions/1
-  # GET /profile_introductions/1.json
-  def show
-  end
+  # # GET /profile_introductions/1
+  # # GET /profile_introductions/1.json
+  # def show
+  # end
 
-  # GET /profile_introductions/new
-  def new
-    @profile_introduction = ProfileIntroduction.new
-  end
+  # # GET /profile_introductions/new
+  # def new
+  #   @profile_introduction = ProfileIntroduction.new
+  # end
 
-  # GET /profile_introductions/1/edit
-  def edit
-  end
+  # # GET /profile_introductions/1/edit
+  # def edit
+  # end
 
   # AJAX purposes only.
   # GET /profile_introduction/swap/1
   def swap
+    check_permission
     respond_to do |format|
       format.js
     end
@@ -34,8 +35,7 @@ class ProfileIntroductionsController < ApplicationController
   # POST /profile_introductions.json
   def create
     @profile_introduction = ProfileIntroduction.new(profile_introduction_params)
-    @profile = @profile_introduction.profile
-    @user_is_owner_of_profile = @profile == current_user.profile
+    check_permission
     respond_to do |format|
       if @profile_introduction.save
         format.html { redirect_to @profile_introduction, notice: 'Profile introduction was successfully created.' }
@@ -51,8 +51,7 @@ class ProfileIntroductionsController < ApplicationController
   # PATCH/PUT /profile_introductions/1
   # PATCH/PUT /profile_introductions/1.json
   def update
-    @profile = @profile_introduction.profile
-    @user_is_owner_of_profile = @profile == current_user.profile
+    check_permission
     respond_to do |format|
       if @profile_introduction.update(profile_introduction_params)
         format.html { redirect_to @profile_introduction, notice: 'Profile introduction was successfully updated.' }
@@ -65,24 +64,29 @@ class ProfileIntroductionsController < ApplicationController
     end
   end
 
-  # DELETE /profile_introductions/1
-  # DELETE /profile_introductions/1.json
-  def destroy
-    @profile_introduction.destroy
-    respond_to do |format|
-      format.html { redirect_to profile_introductions_url, notice: 'Profile introduction was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # # DELETE /profile_introductions/1
+  # # DELETE /profile_introductions/1.json
+  # def destroy
+  #   @profile_introduction.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to profile_introductions_url, notice: 'Profile introduction was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_profile_introduction
-      @profile_introduction = ProfileIntroduction.find(params[:id])
-    end
+  def check_permission
+    @user_is_owner_of_profile = has_profile_permission? @profile_introduction
+    redirect_to @profile_introduction.profile, notice: 'Wrong Permissions' unless @user_is_owner_of_profile
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def profile_introduction_params
-      params.require(:profile_introduction).permit(:profile_id, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_profile_introduction
+    @profile_introduction = ProfileIntroduction.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def profile_introduction_params
+    params.require(:profile_introduction).permit(:profile_id, :body)
+  end
 end
--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,8 @@
 module ProfilesHelper
+
+  # Takes in object that belongs-to profile
+  # Checks whether its profile is the same as the current user's
+  def has_profile_permission?(object)
+    object and object.profile and current_user and object.profile == current_user.profile
+  end
 end
