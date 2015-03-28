c82b4071b3f6e7a234d175c54ca8b291e114afba
Saul Gutierrez <saul.g.gutierrez@gmail.com>
2015-03-28 00:29:48 -0600

    Added coordinates to columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323222025) do
+ActiveRecord::Schema.define(version: 20150328062501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20150323222025) do
     t.string   "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "notifications", force: :cascade do |t|
