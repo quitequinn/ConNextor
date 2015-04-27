58477ae6fa96550a9d1b1e8a46b0f4a05d347cc6
David Song <David.Song0802@gmail.com>
2015-04-27 10:46:48 -0400

    small fix on seed

--- app/models/invitation_code.rb
+++ app/models/invitation_code.rb
@@ -1,5 +1,5 @@
 class InvitationCode < ActiveRecord::Base
-  has_many :invitation_code_records, dependent: :destroy
+  # has_many :invitation_code_records, dependent: :destroy
 
   def self.validate(invitation_code)
     if InvitationCode.find_by_code invitation_code
